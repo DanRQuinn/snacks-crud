1169dc5fe67a0961acba817cb0dc32b005d5a1cb
Daniel Quinn <dandidit.quinn@gmail.com>
2023-07-24 19:58:55 -0700

    full-crud

--- snacks/apps.py
+++ snacks/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class snacks_crud_projectConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'snacks_crud_project'
+    name = 'snacks'
